b7d39941371651b8cc02ba95967ab2cced0b864f
arbazmubasher1 <38607753+arbazmubasher1@users.noreply.github.com>
2025-11-02 16:56:39 +0500

    Address added

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,7 @@ function App() {
     { value: 'Kinnaird College For Women University (CK Delivery Kiosk)', label: 'Kinnaird College For Women University (CK Delivery Kiosk)' },
     { value: 'City School - DHA P6 Campus to JJ Ph6 Branch (Delivery Kiosk)', label: 'City School - DHA P6 Campus to JJ Ph6 Branch (Delivery Kiosk)' },
     { value: 'JT to RAAZ FOOD FEST (Global Village) Delivery Kiosk', label: 'JT to RAAZ FOOD FEST (Global Village) Delivery Kiosk' },
+    { value: 'Cloud KIOSK OPERATIONS - MAANU CONCERT Al-Hamra Cultural Complex, Qaddafi Stadium', label: 'Cloud KIOSK OPERATIONS - MAANU CONCERT Al-Hamra Cultural Complex, Qaddafi Stadium' },
 
   ];
 
